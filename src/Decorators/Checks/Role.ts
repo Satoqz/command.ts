5e24918771d389dc0ea3c573e3610a0256e032e8
Satoqz <satoqzzz@gmail.com>
2020-08-15 20:13:05 +0200

    asd

---
+++ src/Decorators/Checks/Role.ts
@@ -0,0 +1,90 @@
+import * as DJS from "discord.js";
+import { CommandContext } from "../../Interfaces/CommandContext";
+import { isArray } from "util";
+import { CommandArg } from "../../Interfaces/RegisteredCommand";
+
+export class Role
+{
+	/**
+	 * Check for clients/bots permissions
+	 * @param roles ID(s) or role object(s) of roles to be verified
+	 * @returns Forwarded decorator
+	 */
+	public static Client(
+		roles:
+			| DJS.Role
+			| string
+			| (string | DJS.Role)[],
+		lackingPermissionAction?: Function
+	): Function
+	{
+		return roleHelper("client", roles);
+	}
+
+	/**
+	 * Check for users permissions
+	 * @param roles ID(s) or role object(s) of roles to be verified
+	 * @returns Forwarded decorator
+	 */
+	public static User(
+		roles:
+			| DJS.Role
+			| string
+			| (string | DJS.Role)[],
+		lackingPermissionAction?: Function
+	): Function
+	{
+		return roleHelper("user", roles);
+	}
+}
+
+/**
+ * Check for permissions
+ * This is a helper, not for direct use
+ * @param who Whose permissions to check?
+ * @param roles ID(s) or role object(s) of roles to be verified
+ * @returns Decorator
+ * @internal
+ */
+function roleHelper(
+	who: "client" | "user",
+	roles:
+		| DJS.Role
+		| string
+		| (string | DJS.Role)[]
+) : Function
+{
+	return function(parent: Object, name: string | symbol, executor: PropertyDescriptor): PropertyDescriptor
+	{
+		const original = executor.value;
+		executor.value = function(context: CommandContext, ...args: CommandArg[])
+		{
+			const user = who == "client"
+				? context.client.user!
+				: context.author!;
+
+			if (context.channel.type != "dm")
+			{
+				let fail: boolean = false;
+
+				if (!isArray(roles))
+					roles = [roles];
+
+				roles.forEach(i =>
+				{
+					if (typeof(i) == "object")
+						i = i.id;
+
+					if (!context.guild!.member(user)!.roles.cache.has(i))
+					{
+						fail = true;
+						return null;
+					}
+				});
+				if (fail) return null;
+			}
+			return original.apply(this, [context, ...args]);
+		};
+		return executor;
+	};
+}
---
+++ src/Helpers/Internal/ConvertArgs.ts
@@ -0,0 +1,56 @@
+import { CommandContext } from "../../Interfaces/CommandContext";
+import { RegisteredCommand, CommandArg } from "../../Interfaces/RegisteredCommand";
+import { Convert } from "../Exported/Convert";
+/**
+ * Transforms arguments from strings to their real type
+ * @param context The context to be passed to the command
+ * @param command The registered Command, to check the wanted arguments
+ * @param args The arguments specified by the user
+ * @returns The transformed command arguments
+ * @internal
+ */
+export function convertArgs(context: CommandContext, command: RegisteredCommand): CommandArg[]
+{
+	command.argsTypes.forEach((type: CommandArg, index: number) =>
+	{
+		if (context.args[index] != undefined)
+		{
+			switch (type)
+			{
+			case "number":
+				context.args[index] = Number(context.args[index]);
+				break;
+
+			case "boolean":
+				const lower = String(context.args[index]).toLowerCase();
+				const bool = ["true", "yes", "y"].includes(lower) ? true : ["false", "no", "n"].includes(lower) ? false : undefined;
+				context.args[index] = bool;
+				break;
+
+			case "string":
+				break;
+
+			case "user":
+				context.args[index] = Convert.toUser(context.args[index] as string, context.c);
+				break;
+
+			case "guildmember":
+				context.args[index] = Convert.toMember(context.args[index] as string, context.guild);
+				break;
+
+			case "textchannel":
+				context.args[index] = Convert.toChannel(context.args[index] as string, context.guild);
+				break;
+
+			case "role":
+				context.args[index] = Convert.toRole(context.args[index] as string, context.guild);
+				break;
+
+			case "infinite":
+				context.args[index] = context.args.slice(index).join(" ");
+				break;
+			}
+		}
+	});
+	return context.args;
+}
